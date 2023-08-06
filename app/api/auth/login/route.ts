3e3c0dc52ccbe006293b33063a3466e9bfdadbfc
Brian <noh24@ymail.com>
2023-08-06 19:26:01 -0400

    create login api route

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,26 @@
+import { NextApiResponse, NextApiRequest } from 'next/types'
+import prisma from '@/db'
+import bcrypt from 'bcrypt'
+
+export default async function POST(req: NextApiRequest, res: NextApiResponse) {
+  const { email, password } = req.body
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email
+      }
+    })
+
+    if (!user) throw new Error('Invalid email!')
+
+    const validPassword = bcrypt.compareSync(password, user.password!)
+
+    if (!validPassword) throw new Error('Invalid password!')
+
+    res.status(200).json({ message: 'Login successful.' })
+    return user
+  } catch (error) {
+    console.error(error)
+  }
+}
