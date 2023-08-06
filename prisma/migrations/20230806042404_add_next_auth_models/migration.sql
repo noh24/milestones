f5490eeee6ed13fa2d261152cd3e4974d89fcf8d
Brian <noh24@ymail.com>
2023-08-06 00:24:45 -0400

    add next auth prisma model

---
+++ prisma/migrations/20230806042404_add_next_auth_models/migration.sql
@@ -0,0 +1,85 @@
+/*
+  Warnings:
+
+  - The primary key for the `milestone` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `filePath` on the `milestone` table. All the data in the column will be lost.
+  - The primary key for the `user` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `password` on the `user` table. All the data in the column will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `milestone` DROP FOREIGN KEY `Milestone_userId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `profile` DROP FOREIGN KEY `Profile_userId_fkey`;
+
+-- AlterTable
+ALTER TABLE `milestone` DROP PRIMARY KEY,
+    DROP COLUMN `filePath`,
+    ADD COLUMN `document` VARCHAR(191) NULL,
+    MODIFY `id` VARCHAR(191) NOT NULL,
+    MODIFY `userId` VARCHAR(191) NOT NULL,
+    ADD PRIMARY KEY (`id`);
+
+-- AlterTable
+ALTER TABLE `profile` MODIFY `userId` VARCHAR(191) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `user` DROP PRIMARY KEY,
+    DROP COLUMN `password`,
+    ADD COLUMN `emailVerified` DATETIME(3) NULL,
+    ADD COLUMN `image` VARCHAR(191) NULL,
+    MODIFY `id` VARCHAR(191) NOT NULL,
+    ADD PRIMARY KEY (`id`);
+
+-- CreateTable
+CREATE TABLE `Account` (
+    `id` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `provider` VARCHAR(191) NOT NULL,
+    `providerAccountId` VARCHAR(191) NOT NULL,
+    `refresh_token` TEXT NULL,
+    `access_token` TEXT NULL,
+    `expires_at` INTEGER NULL,
+    `token_type` VARCHAR(191) NULL,
+    `scope` VARCHAR(191) NULL,
+    `id_token` TEXT NULL,
+    `session_state` VARCHAR(191) NULL,
+    `userId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Account_provider_providerAccountId_key`(`provider`, `providerAccountId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Session` (
+    `id` VARCHAR(191) NOT NULL,
+    `sessionToken` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Session_sessionToken_key`(`sessionToken`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `VerificationToken` (
+    `identifier` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `VerificationToken_token_key`(`token`),
+    UNIQUE INDEX `VerificationToken_identifier_token_key`(`identifier`, `token`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Account` ADD CONSTRAINT `Account_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Session` ADD CONSTRAINT `Session_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Milestone` ADD CONSTRAINT `Milestone_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Profile` ADD CONSTRAINT `Profile_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
