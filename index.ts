7125f274ec4e75a18999418e61726880d1521475
Brian <noh24@ymail.com>
2023-08-05 20:35:08 -0400

    testing prisma db queries

--- index.ts
+++ index.ts
@@ -3,36 +3,17 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 const main = async () => {
-  const milestone = await prisma.milestone.update({
-    where: { id: 1 },
-    data: { content: 'first database entry and update'},
-  })
-  // await prisma.user.create({
-  //   data: {
-  //     name: 'dummy',
-  //     email: 'dummy@test.com',
-  //     password: 'abc123',
-  //     milestones: {
-  //       create: {
-  //         title: 'my first prisma connection with next js',
-  //         content: 'omg, i finally connected a database on nextjs'
-  //       },
-  //     },
-  //     profile: {
-  //       create: {
-  //         profession: 'swe bro'
-  //       },
-  //     },
-  //   },
+  // const allUsers = await prisma.user.findMany({
+  //   include: {
+  //     milestones: true,
+  //     profile: true,
+  //   }
   // })
 
-  const allUsers = await prisma.user.findMany({
-    include: {
-      milestones: true,
-      profile: true,
-    }
-  })
-  console.dir(allUsers, { depth: null})
+  const milestones = await prisma.milestone.findMany({})
+
+  // console.dir(allUsers, { depth: null })
+  console.dir(milestones, { depth: null })
 }
 
 main()
