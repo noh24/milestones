72facb5a21b27858ea3dfde9819a3695bab58ad7
Brian <noh24@ymail.com>
2023-08-06 21:55:01 -0400

    use api router for next auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,51 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from '@/db'
+import bcrypt from 'bcrypt'
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text', placeholder: 'johnsmith@example.com' },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials!
+
+        try {
+          const user = await prisma.user.findFirst({
+            where: {
+              email,
+            }
+          })
+
+          if (!user) throw new Error('Invalid email!')
+
+          const validPassword = bcrypt.compareSync(password, user.password!)
+          if (!validPassword) throw new Error('Invalid password!')
+
+          return user
+        } catch (error) {
+          console.error('Error occured: ', error)
+          return null
+        }
+      },
+    })
+  ],
+  pages: {
+    signIn: '/login'
+  }
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
