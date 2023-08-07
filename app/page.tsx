4ae02d96f383bc5be751c8fa2b7f84da077d05c2
Brian <noh24@ymail.com>
2023-08-06 21:58:52 -0400

    fetch server session on server side

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
-import Link from "next/link";
+import Link from 'next/link'
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from './api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
 
-export default function Home() {
+export default async function Home() {
+  await getSession()
+
   return (
     <>
       <Link href='/about'>About</Link>
-      <div className="text-5xl">Home Page</div>
+      <div className='text-5xl'>Home Page</div>
     </>
   )
+}
+
+async function getSession() {
+  const session = await getServerSession(authOptions)
+  console.log('asdasd', session)
+  if (!session) {
+    redirect('/login?callbackUrl=/')
+  }
+
+  return {
+    props: {
+      session,
+    },
+  }
 }
