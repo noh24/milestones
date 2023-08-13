d10eacf421e7d290823e2fe1ddc97cd5aa5d9e96
Brian <noh24@ymail.com>
2023-08-13 00:58:30 -0400

    add global ExtendedRequest interface

--- types/global-types.d.ts
+++ types/global-types.d.ts
@@ -1,12 +1,10 @@
 // Next Auth Providers
 type ProvidersType = Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> | null
 
-type User = {
-  id: string
-  name: string
-  email: string
-  isAdmin: boolean
-  password: string | null
-  emailVerified: Date | null
-  image: string | null
+interface ExtendedRequest extends Request {
+  body: ReadableStream<Uint8Array> & {
+    name?: string
+    email: string
+    password: string
+  }
 }
