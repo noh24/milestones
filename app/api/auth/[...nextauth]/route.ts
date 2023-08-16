fe683ac206e495844f0b9e291f8a09d806efcac5
Brian <noh24@ymail.com>
2023-08-15 23:59:51 -0400

    remove try catch block

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,11 @@
-import NextAuth from 'next-auth'
+import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import prisma from '@/db'
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
+  session: { strategy: 'jwt'},
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -12,38 +13,27 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
     CredentialsProvider({
-      name: 'Credentials',
       credentials: {
-        email: { label: 'Email', type: 'text', placeholder: 'johnsmith@example.com' },
+        email: { label: 'Email', type: 'text' },
         password: { label: 'Password', type: 'password' }
       },
       async authorize(credentials) {
         const { email, password } = credentials!
 
-        try {
-          console.log('before fetch')
-          const response = await fetch('http:localhost:3000/api/auth/login', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              email,
-              password,
-            })
+        const response = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/login`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email,
+            password,
           })
-          console.log('after fetch')
+        })
 
-          const data = await response.json()
-          console.log('t========', data)
+        const data = await response.json()
 
-          if (!response.ok) throw new Error(data.message)
-
-          return data.response
-        } catch (error) {
-          console.log(`Error!!!!: ${error}`)
-          // not returning error bc nextAuth signIn has its own return object
-        }
+        return data.response
       },
     })
   ],
