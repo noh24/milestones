198de564381160bdfeb4f42923de4f3c6db9d96b
Brian <noh24@ymail.com>
2023-08-19 19:14:33 -0400

    display sign in or out based on session data

--- app/components/SignOutButton.tsx
+++ app/components/SignOutButton.tsx
@@ -1,25 +1,18 @@
 'use client'
 
-import { signOut, useSession } from "next-auth/react"
-import React, { useEffect, useState } from "react";
+import { signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+import React, { FC } from 'react'
 
-const SignOutButton = () => {
-  const session = useSession()
-  console.log(session)
-  
-  const [signInOrOut, setSignInOrOut] = useState('')
-  
-  useEffect(() => {
-    console.log(session)
-    if (session.status !== 'authenticated') {
-      setSignInOrOut('Sign In')
-    } else {
-      setSignInOrOut('Sign Out')
-    }
-  }, [session])
+const SignOutButton: FC = () => {
+  const { data: session } = useSession()
+  const router = useRouter()
 
+  return session ? (
+    <button onClick={() => signOut()}>Sign Out</button>
+  ) : (
+    <button onClick={() => router.push('/signin')}>Sign In</button>
+  )
+}
 
-  return <button onClick={() => signOut()}>{signInOrOut}</button>
-};
-
-export default SignOutButton;
+export default SignOutButton
