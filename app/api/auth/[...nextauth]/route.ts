99e4553172c11a65fa8bc682f55b7d956fc8a3be
Brian <noh24@ymail.com>
2023-08-19 19:04:20 -0400

    allow automatic email account linking for google oauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -12,6 +12,7 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
     CredentialsProvider({
       credentials: {},
@@ -37,25 +38,8 @@ export const authOptions: NextAuthOptions = {
           throw new Error(data.message)
         }
       },
-    })
+    }),
   ],
-  // callbacks: {
-  //   async jwt({ token, user }) {
-  //     if (user) {
-  //       token.id = user.id
-  //     }
-  //     return token
-  //   },
-  //   async session({ session, token, user }) {
-  //     if (session.user) {
-  //       session.user.id = token.id as string
-  //       // session.user.name = `${token.email}`
-  //       // session.user.email = user.email as string
-  //       // session.user.image = user.image as string
-  //     }
-  //     return session
-  //   }
-  // },
   pages: {
     signIn: '/signin'
   }
