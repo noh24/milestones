b72a5e2684cba0e2f0d5bf6a1da6983a2d33ec42
Brian <noh24@ymail.com>
2023-08-20 19:36:27 -0400

    set callback url for signOut() to signin page

--- app/components/SignOutButton.tsx
+++ app/components/SignOutButton.tsx
@@ -9,7 +9,15 @@ const SignOutButton: FC = () => {
   const router = useRouter()
 
   return session ? (
-    <button onClick={() => signOut()}>Sign Out</button>
+    <button
+      onClick={() =>
+        signOut({
+          callbackUrl: `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/signin`,
+        })
+      }
+    >
+      Sign Out
+    </button>
   ) : (
     <button onClick={() => router.push('/signin')}>Sign In</button>
   )
