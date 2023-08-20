264b28ada7d0c56d2b403b4cf104362fa5eecd52
Brian <noh24@ymail.com>
2023-08-19 20:27:39 -0400

    add milestones logo to header

--- app/components/HeaderNavBar.tsx
+++ app/components/HeaderNavBar.tsx
@@ -1,13 +1,32 @@
-import Link from "next/link"
-import React, { FC } from "react";
-import SignOutButton from "./SignOutButton"
+import Link from 'next/link'
+import React, { FC } from 'react'
+import SignOutButton from './SignOutButton'
+import Image from 'next/image'
 
 const HeaderNavBar: FC = () => {
-  return <nav>
-    <Link href='/'>Home</Link>
-    <Link href='/get-started'>Get Started</Link>
-    <SignOutButton/>
-  </nav>
-};
+  return (
+    <header>
+      <nav>
+        <Link href='/'>
+          <Image
+            src='/milestones-logo.png'
+            alt='milestones logo'
+            width={50}
+            height={50}
+          ></Image>{' '}
+          Milestones
+        </Link>
+        <ul>
+          <li>
+            <Link href='/get-started'>Get Started</Link>
+          </li>
+          <li>
+            <SignOutButton />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
 
-export default HeaderNavBar;
+export default HeaderNavBar
