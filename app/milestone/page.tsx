4aaff1aed3e787a96031129a4b5cb0e62ab7afbd
Brian <noh24@ymail.com>
2023-08-20 22:26:13 -0400

    wip: add uploaded images previews, pdf docs need work

--- app/milestone/page.tsx
+++ app/milestone/page.tsx
@@ -4,6 +4,7 @@ import React, { FC, useEffect } from 'react'
 import { useSession } from 'next-auth/react'
 import { redirect } from 'next/navigation'
 import { useState, useCallback } from 'react'
+import Image from 'next/image'
 
 type MilestoneData = {
   title: string
@@ -24,8 +25,18 @@ const Milestone: FC = () => {
 
   const [milestoneData, setMilestoneData] =
     useState<MilestoneData>(initialMilestoneData)
+  const [preview, setPreview] = useState<string>('')
 
   useEffect(() => console.log(milestoneData), [milestoneData])
+
+  useEffect(() => {
+    if (milestoneData.document && milestoneData.document.length != 0) {
+      console.log(milestoneData.document[0])
+      const fileURL = URL.createObjectURL(milestoneData.document[0])
+      setPreview(() => fileURL)
+    }
+  }, [milestoneData.document])
+
   const updateMilestoneDataHandler = useCallback(
     (type: keyof MilestoneData) =>
       (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
@@ -33,6 +44,14 @@ const Milestone: FC = () => {
           ...prevState,
           [type]: event.target.value,
         })),
+    []
+  )
+  const updateMilestoneDocumentHandler = useCallback(
+    () => (event: React.ChangeEvent<HTMLInputElement>) =>
+      setMilestoneData((prevState) => ({
+        ...prevState,
+        document: event.target.files,
+      })),
     []
   )
 
@@ -44,11 +63,13 @@ const Milestone: FC = () => {
           name='title'
           type='text'
           placeholder='Title'
+          value={milestoneData.title}
           onChange={updateMilestoneDataHandler('title')}
         />
         <textarea
           name='content'
           placeholder='Enter milestone details'
+          value={milestoneData.content}
           onChange={updateMilestoneDataHandler('content')}
         />
         <label>
@@ -75,9 +96,16 @@ const Milestone: FC = () => {
             name='document'
             type='file'
             accept='.doc,.docx,.pdf,image/*'
-            onChange={updateMilestoneDataHandler('document')}
+            onChange={updateMilestoneDocumentHandler()}
           />
         </label>
+        <div>
+          {preview ? (
+            <Image src={preview} alt='' width={300} height={500} />
+          ) : (
+            <div></div>
+          )}
+        </div>
       </form>
     </>
   )
