5a17794e782b7b368ac7ca950fa04d217174dc84
Brian <noh24@ymail.com>
2023-08-22 23:37:33 -0400

    remove extended request interface

--- types/global-types.d.ts
+++ types/global-types.d.ts
@@ -1,10 +1,2 @@
 // Next Auth Providers
 type ProvidersType = Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> | null
-
-interface ExtendedRequest extends Request {
-  body: ReadableStream<Uint8Array> & {
-    name?: string
-    email: string
-    password: string
-  }
-}
