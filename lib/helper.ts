a4b68ca72edf3f3e4fe9c1da5d96a2b3b1b0baf4
Brian <noh24@ymail.com>
2023-08-24 02:08:51 -0400

    add confirmPassword static method

--- lib/helper.ts
+++ lib/helper.ts
@@ -3,6 +3,10 @@ import crypto from 'crypto'
 
 export default class Helper {
 
+  static confirmPassword(userData: UserData): boolean {
+    return userData.password === userData.confirmPassword
+  }
+
   static validateType(type: string): boolean {
     const lowerCaseType = type.toLowerCase()
     const mimeTypes = [
