e5f7346a6da724cf774e92f325e7ae5948860bc1
Brian <noh24@ymail.com>
2023-08-24 02:08:36 -0400

    add UserData + MilestoneData types

--- types/global-types.d.ts
+++ types/global-types.d.ts
@@ -1,2 +1,17 @@
 // Next Auth Providers
 type ProvidersType = Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> | null
+
+type UserData = {
+  name: string
+  email: string
+  password: string
+  confirmPassword: string
+}
+
+type MilestoneData = {
+  title: string
+  content: string
+  type: string
+  date: string
+  document?: File
+}
