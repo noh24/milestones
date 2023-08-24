cc6051fbcdc087159688ae22fbf7c0f03b223de7
Brian <noh24@ymail.com>
2023-08-24 01:55:10 -0400

    clarify api response to success or error

--- app/api/milestones/route.ts
+++ app/api/milestones/route.ts
@@ -43,9 +43,9 @@ export async function POST(req: Request) {
       }
     })
 
-    return NextResponse.json({ message: 'Successfully created a milestone', data: newMilestone }, { status: 200 })
+    return NextResponse.json({ success: 'Successfully created a milestone', data: newMilestone }, { status: 200 })
   } catch (err) {
     console.log(err)
-    return NextResponse.json({ message: `${err}` }, { status: 400 })
+    return NextResponse.json({ error: `${err}` }, { status: 400 })
   }
 }
