881d3dc355fe72fcd79beaa2e016011b9ed029cf
Brian <noh24@ymail.com>
2023-08-24 01:36:20 -0400

    create Helper class with static methods

--- lib/helper.ts
+++ lib/helper.ts
@@ -1,11 +1,24 @@
-export const validateType = (type: string): boolean => {
-  const lowerCaseType = type.toLowerCase()
-  const mimeTypes = [
-    'application/msword',
-    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-    'application/pdf',
-    'image/jpeg',
-    'image/png',
-  ]
-  return mimeTypes.includes(lowerCaseType)
+import path from 'path'
+import crypto from 'crypto'
+
+export default class Helper {
+
+  static validateType(type: string): boolean {
+    const lowerCaseType = type.toLowerCase()
+    const mimeTypes = [
+      'application/msword',
+      'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      'application/pdf',
+      'image/jpeg',
+      'image/png',
+    ]
+    return mimeTypes.includes(lowerCaseType)
+  }
+
+  static generateUploadPath = (): string => {
+    const randomId = crypto.randomUUID()
+    const year = new Date().getFullYear()
+    const month = new Date().getMonth()
+    return path.join(__dirname, '..', '..', '..', 'uploads', String(year), String(month), randomId)
+  }
 }
