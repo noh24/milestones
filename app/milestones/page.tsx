94a3f1c0878b8a4c7de6157a339035aaf23ac738
Brian <noh24@ymail.com>
2023-08-30 23:22:05 -0400

    wip: fetch milestones data

--- app/milestones/page.tsx
+++ app/milestones/page.tsx
@@ -1,7 +1,26 @@
+import { getServerSession } from 'next-auth'
 import React, { FC } from 'react'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 
-const Milestones: FC = () => {
+const Milestones: FC = async () => {
+  const session = await getSession()
+  const data = await getMilestones(session?.user?.email!)
   return <div>Milestones</div>
 }
 
 export default Milestones
+
+const getSession = async () => {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    return
+  }
+  return session
+}
+
+const getMilestones = async (email: string) => {
+  const res = await fetch('http://localhost:3000/api/milestones')
+
+  return res.json()
+}
