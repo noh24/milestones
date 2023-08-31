8a5f795f9a2868542fea1a51a0d424a4350fa1d6
Brian <noh24@ymail.com>
2023-08-30 23:43:06 -0400

    standardize api response objects

--- app/api/milestones/route.ts
+++ app/api/milestones/route.ts
@@ -6,7 +6,7 @@ import { getServerSession } from "next-auth"
 import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 
 // Get
-export async function GET(req: Request) {
+export async function GET() {
   try {
     const session = await getServerSession(authOptions)
     if (!session) throw new Error('You must login')
@@ -22,51 +22,49 @@ export async function GET(req: Request) {
     return NextResponse.json({ success: true, data: milestones, error: null }, { status: 200 })
   } catch (err) {
     console.log('Milestones Get Error: ', err)
-    return NextResponse.json({ success: false, data: null, error: `${err}` }, { status: 400 })
+    return NextResponse.json({ success: false, data: null, error: String(err) }, { status: 400 })
   }
 }
 
-
 // Post
 export async function POST(req: Request) {
-  const formData = await req.formData()
-  const title = formData.get('title') as string
-  const content = formData.get('content') as string
-  const type = formData.get('type') as string
-  const date = new Date(formData.get('date') as string).toISOString()
-  const userEmail = formData.get('userEmail') as string
-  const document: File | null = formData.get('document') as File
+  const res = await req.formData()
+  const userEmail = res.get('userEmail') as string
+  const formData = {
+    title: res.get('title') as string,
+    content: res.get('content') as string,
+    type: res.get('type') as string,
+    date: new Date(res.get('date') as string).toISOString(),
+    document: res.get('document') as File | null,
+  }
 
   try {
     const user = await prisma.user.findFirstOrThrow({ where: { email: userEmail }, select: { id: true } })
 
     let documentPath: string | null = null
 
-    if (document) {
-      const isValid = Helper.validateType(document?.type)
+    if (formData.document) {
+      const isValid = Helper.validateType(formData.document?.type)
       if (!isValid) throw new Error('Document type is not acceptable MIME type.')
 
-      const arrayBuffer = await document.arrayBuffer()
+      const arrayBuffer = await formData.document.arrayBuffer()
       const buffer = Buffer.from(arrayBuffer)
 
-      documentPath = Helper.generateUploadPath() + Helper.generateFileExtension(document.type)
+      documentPath = Helper.generateUploadPath() + Helper.generateFileExtension(formData.document.type)
       await writeFile(documentPath, buffer)
     }
 
     const newMilestone = await prisma.milestone.create({
       data: {
-        title,
-        content,
-        type,
-        date,
+        ...formData,
         userId: user.id,
         document: documentPath
       }
     })
 
-    return NextResponse.json({ success: 'Successfully created a milestone', data: newMilestone }, { status: 200 })
+    return NextResponse.json({ success: true, data: newMilestone, error: null }, { status: 200 })
   } catch (err) {
     console.log('Milestone Add Route', err)
-    return NextResponse.json({ error: `${err}` }, { status: 400 })
+    return NextResponse.json({ success: false, data: null, error: String(err) }, { status: 400 })
   }
 }
