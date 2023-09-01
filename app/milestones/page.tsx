16aa90b30fa5831e8da99fa6b81e33f6804a45fc
Brian <noh24@ymail.com>
2023-09-01 00:11:32 -0400

    wip: display user associated milestones

--- app/milestones/page.tsx
+++ app/milestones/page.tsx
@@ -1,26 +1,68 @@
 import { getServerSession } from 'next-auth'
 import React, { FC } from 'react'
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
+import type { Session } from 'next-auth'
+import type { Milestone } from '@prisma/client'
+import prisma from '@/db'
 
 const Milestones: FC = async () => {
-  const session = await getSession()
-  const data = await getMilestones(session?.user?.email!)
-  return <div>Milestones</div>
+  const data = await getMilestones()
+
+  return (
+    <>
+      {data instanceof Error ? (
+        <div>{String(data)}</div>
+      ) : (
+        <div>
+          {data.map(({ id, title, content, type, document, date }) => (
+            <div key={id}>
+              <h2>{title}</h2>
+              <p>{content}</p>
+              <p>{type}</p>
+              <p>{date.toDateString()}</p>
+              <p>{document}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  )
 }
 
 export default Milestones
 
-const getSession = async () => {
+const getSession = async (): Promise<Session> => {
   const session = await getServerSession(authOptions)
+  console.log('milestones page session: ', session)
 
   if (!session) {
-    return
+    redirect('/signin?redirect=milestones')
   }
   return session
 }
 
-const getMilestones = async (email: string) => {
-  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/milestones`)
+const getMilestones = async (): Promise<Milestone[] | Error> => {
+  const session = await getSession()
+  try {
+    const user = await prisma.user.findFirstOrThrow({
+      where: {
+        email: session.user?.email!,
+      },
+      select: {
+        id: true,
+      },
+    })
 
-  return res.json()
+    const milestones = await prisma.milestone.findMany({
+      where: {
+        userId: user.id,
+      },
+    })
+
+    return milestones
+  } catch (err) {
+    console.log('milestones page user:', err)
+    return err as Error
+  }
 }
