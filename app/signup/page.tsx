d1b07d6ca7d7227ed458e46b1b5a767c3fd8c62e
Brian <noh24@ymail.com>
2023-09-02 15:03:19 -0400

    combine separate useState into one localState

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -6,8 +6,8 @@ import { useRouter } from 'next/navigation'
 import Helper from '@/lib/helper'
 
 const SignUp = () => {
+  const { status } = useSession()
   const router = useRouter()
-  const { data: session, status } = useSession()
 
   useEffect(() => {
     if (status === 'authenticated') router.push('/')
@@ -20,101 +20,117 @@ const SignUp = () => {
     confirmPassword: '',
   })
 
-  const [error, setError] = useState<boolean>(false)
-  const [message, setMessage] = useState<string>('')
-  const [loading, setLoading] = useState<boolean>(false)
-
-  const updateUserDataHandler = useCallback(
-    (type: keyof UserSignUpData) => (event: React.ChangeEvent<HTMLInputElement>) => {
-      setUserData((prevData) => ({ ...prevData, [type]: event.target.value }))
-    },
-    []
-  )
+  const [localState, setLocalState] = useState({
+    loading: false,
+    success: false,
+    error: false,
+    message: '',
+  })
 
   useEffect(() => {
-    if (Helper.confirmPassword(userData)) {
-      setError((prevState) => !prevState)
-    } else {
-      setError((prevState) => !prevState)
-    }
+    setLocalState((prevState) => ({
+      ...prevState,
+      error: userData.password !== userData.confirmPassword,
+    }))
   }, [userData])
 
-  const formHandler = useCallback(
-    () => async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault()
+  const formHandler = () => async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setLoading((prevState) => !prevState)
+    setMessage('')
+
+    try {
+      const response = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: userData.name,
+          email: userData.email,
+          password: userData.password,
+        }),
+      })
       setLoading((prevState) => !prevState)
-      setMessage('')
 
-      try {
-        const response = await fetch('/api/auth/signup', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            name: userData.name,
-            email: userData.email,
-            password: userData.password,
-          }),
-        })
-        setLoading((prevState) => !prevState)
+      const data = await response.json()
 
-        const data = await response.json()
+      if (!response.ok) throw new Error(data.error)
 
-        if (!response.ok) throw new Error(data.error)
+      setMessage(data.success)
 
-        setMessage(data.success)
-
-        setTimeout(() => router.push('/signin'), 3000)
-      } catch (error) {
-        setMessage(`${error}`)
-      }
-    },
-    [userData, router]
-  )
+      setTimeout(() => router.push('/signin'), 3000)
+    } catch (error) {
+      setMessage(`${error}`)
+    }
+  }
 
   return (
     <>
-      <form onSubmit={formHandler()}>
+      <form onSubmit={formHandler}>
         <input
-          name='full-name'
+          name='name'
           type='text'
-          placeholder='Enter your full name'
+          placeholder='Enter your name'
           required
           value={userData.name}
-          onChange={updateUserDataHandler('name')}
+          onChange={(e) =>
+            setUserData((prevState) => ({
+              ...prevState,
+              name: e.target.value,
+            }))
+          }
         />
         <input
           name='email'
-          type='text'
+          type='email'
           placeholder='Enter your email'
           required
           value={userData.email}
-          onChange={updateUserDataHandler('email')}
+          onChange={(e) =>
+            setUserData((prevState) => ({
+              ...prevState,
+              email: e.target.value,
+            }))
+          }
         />
         <input
-          name='current-password'
+          name='password'
           type='password'
           placeholder='Enter your password'
           required
           value={userData.password}
-          onChange={updateUserDataHandler('password')}
+          onChange={(e) =>
+            setUserData((prevState) => ({
+              ...prevState,
+              password: e.target.value,
+            }))
+          }
         />
         <input
-          name='confirm-password'
+          name='confirmPassword'
           type='password'
           placeholder='Re-enter your password'
           required
           value={userData.confirmPassword}
-          onChange={updateUserDataHandler('confirmPassword')}
+          onChange={(e) =>
+            setUserData((prevState) => ({
+              ...prevState,
+              confirmPassword: e.target.value,
+            }))
+          }
         />
-        <button type='submit' disabled={error || loading}>
+        <button type='submit' disabled={localState.error || localState.loading}>
           Create account
         </button>
       </form>
       <div>
-        <p className={message ? 'block' : 'hidden'}>{message}</p>
-        <p className={error ? 'block' : 'hidden'}>Passwords must match!</p>
+        <p className={localState.message ? 'block' : 'hidden'}>
+          {localState.message}
+        </p>
+        <p className={localState.error ? 'block' : 'hidden'}>
+          Passwords must match!
+        </p>
       </div>
     </>
   )
