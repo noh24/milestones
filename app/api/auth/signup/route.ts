7a5e089ceefcdb5002bf507cdcf82d2201a65d0b
Brian <noh24@ymail.com>
2023-09-02 21:46:21 -0400

    add confirm password check and sanitize error msg

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,11 +1,14 @@
 import bcrypt from 'bcrypt'
 import prisma from '@/db'
 import { NextResponse } from 'next/server'
+import Helper from '@/lib/helper'
 
 export async function POST(req: Request): Promise<NextResponse<SignUpAPIResponse>> {
-  const { name, email, password } = await req.json()
+  const { name, email, password, confirmPassword } = await req.json()
 
   try {
+    if (password !== confirmPassword) throw new Error('Passwords must match!')
+
     const existingUser = await prisma.user.findFirst({
       where: {
         email
@@ -15,7 +18,7 @@ export async function POST(req: Request): Promise<NextResponse<SignUpAPIResponse
       }
     })
 
-    if (existingUser) throw new Error(`${email} has already been used.`)
+    if (existingUser) throw new Error(`Email provided is already in use: ${email}`)
 
     const hashedPassword = bcrypt.hashSync(password, 10)
     await prisma.user.create({
@@ -29,6 +32,6 @@ export async function POST(req: Request): Promise<NextResponse<SignUpAPIResponse
     return NextResponse.json({ success: true, data: 'Successfully registered user.', error: null }, { status: 200 })
   } catch (err) {
     console.error('Sign Up Route: ', err)
-    return NextResponse.json({ success: false, data: null, error: String(err) }, { status: 400 })
+    return NextResponse.json({ success: false, data: null, error: Helper.sanitizeErrorMessage(String(err)) }, { status: 400 })
   }
 }
