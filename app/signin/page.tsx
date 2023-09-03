7b2b86712f87a2d59cc8e2f67d15d9f8c4d14e72
Brian <noh24@ymail.com>
2023-09-03 00:52:23 -0400

    remove usecallback

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { signIn, useSession } from 'next-auth/react'
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import Link from 'next/link'
 import { useQuery } from '@tanstack/react-query'
@@ -25,55 +25,50 @@ const SignIn = () => {
   const [loading, setLoading] = useState<boolean>(false)
   const [message, setMessage] = useState<string>('')
 
-  const updateUserDataHandler = useCallback(
+  const updateUserData =
     (type: keyof UserSignInData) =>
-      (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUserData({ ...userData, [type]: event.target.value })
-      },
-    [userData]
-  )
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setUserData({ ...userData, [type]: event.target.value })
+    }
 
-  const formHandler = useCallback(
-    () => async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault()
-      setLoading((prevState) => !prevState)
-      setMessage('')
+  const formHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setLoading((prevState) => !prevState)
+    setMessage('')
 
-      const result = await signIn('credentials', {
-        email: userData.email,
-        password: userData.password,
-        callbackUrl: '/',
-        redirect: false,
-      })
+    const result = await signIn('credentials', {
+      email: userData.email,
+      password: userData.password,
+      callbackUrl: '/',
+      redirect: false,
+    })
 
-      setLoading((prevState) => !prevState)
+    setLoading((prevState) => !prevState)
 
-      if (result?.error) {
-        setSignInError(true)
-        setMessage('The password or email you have entered is invalid.')
-      } else {
-        router.push(`${result?.url!}/${redirect}`)
-      }
-    },
-    [userData, router, redirect]
-  )
+    if (result?.error) {
+      setSignInError(true)
+      setMessage('The password or email you have entered is invalid.')
+    } else {
+      router.push(`${result?.url!}/${redirect}`)
+    }
+  }
 
   return (
     <>
-      <form onSubmit={formHandler()}>
+      <form onSubmit={formHandler}>
         <input
           name='email'
           type='text'
           placeholder='Enter Email'
           value={userData.email}
-          onChange={updateUserDataHandler('email')}
+          onChange={updateUserData('email')}
         />
         <input
           name='current-password'
           type='password'
           placeholder='Enter Password'
           value={userData.password}
-          onChange={updateUserDataHandler('password')}
+          onChange={updateUserData('password')}
         />
         <button type='submit' disabled={loading}>
           Sign in with Credentials
