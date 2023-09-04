83cad4534cf9232ab95bf5d0cbae6541825b5804
Brian <noh24@ymail.com>
2023-09-04 16:17:39 -0400

    replace formHandler() with react query useMutation()

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,16 +1,16 @@
 'use client'
 
 import { signIn, useSession } from 'next-auth/react'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import Link from 'next/link'
 import { useMutation, useQuery } from '@tanstack/react-query'
 import OAuthProviders from './OAuthProviders'
 
 const SignIn = () => {
+  const { status } = useSession()
   const router = useRouter()
   const redirect = useSearchParams().get('redirect') ?? ''
-  const { status } = useSession()
 
   useEffect(() => {
     if (status === 'authenticated') router.push(`/${redirect}`)
@@ -31,40 +31,37 @@ const SignIn = () => {
       setUserData({ ...userData, [type]: event.target.value })
     }
 
-  const formHandler = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    setLoading((prevState) => !prevState)
-    setMessage('')
-
-    const result = await signIn('credentials', {
-      email: userData.email,
-      password: userData.password,
-      callbackUrl: '/',
-      redirect: false,
-    })
-
-    setLoading((prevState) => !prevState)
-
-    if (result?.error) {
-      setSignInError(true)
-      setMessage('The password or email you have entered is invalid.')
-    } else {
-      router.push(`${result?.url!}/${redirect}`)
-    }
-  }
-
-  const query = useMutation({
-    mutationFn: () => {
-      //fill in
+  const mutation = useMutation({
+    mutationFn: async () => {
+      const res = await signIn('credentials', {
+        email: userData.email,
+        password: userData.password,
+        callbackUrl: '/',
+        redirect: false,
+      })
+      console.log('signIn res: ', res)
+      if (res?.error) {
+        throw new Error(String(res?.error))
+      } else {
+        router.push(`${res?.url}/${redirect}`)
+      }
     },
   })
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    mutation.mutate()
   }
+
+  const countRef = useRef(0)
+  useEffect(() => {
+    countRef.current++
+    console.log(countRef, mutation)
+  }, [mutation])
+
   return (
     <>
-      <form onSubmit={formHandler}>
+      <form onSubmit={onSubmit}>
         <input
           name='email'
           type='text'
