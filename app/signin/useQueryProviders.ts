a67f17371a9b11a916cc749a33d2e5a146c265f8
Brian <noh24@ymail.com>
2023-09-04 16:35:32 -0400

    change file name to useQueryProviders

---
+++ app/signin/useQueryProviders.ts
@@ -0,0 +1,36 @@
+import { useQuery } from '@tanstack/react-query'
+import { getProviders, signIn } from 'next-auth/react'
+import Loading from '@/app/loading'
+
+const useQueryProviders = () => {
+  const providers = useQuery({
+    queryKey: ['providers'],
+    queryFn: async () => {
+      const providers = await getProviders()
+      if (!providers) throw new Error('No Providers Available')
+      return providers
+    },
+    select: (data) =>
+      Object.values(data).filter((provider) => provider.id !== 'credentials'),
+  })
+
+  return providers
+
+
+  // if (providers.isLoading) return <Loading />
+  // return (
+  //   <>
+  //     {providers.data &&
+  //       providers.data.map((provider) => (
+  //         <div key={provider.name}>
+  //           <button onClick={() => signIn(provider.id)}>
+  //             Sign in with {provider.name}
+  //           </button>
+  //         </div>
+  //       ))}
+  //     <p>{providers.error ? (providers.error as Error).message : null}</p>
+  //   </>
+  // )
+}
+
+export default useQueryProviders
