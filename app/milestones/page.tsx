9e6a12def9dc8403d6ec785cb6d0198e8ca054ea
Brian <brian.noh95@gmail.com>
2023-09-06 00:51:34 -0400

    add conditional rendering based on error, success, & data length

--- app/milestones/page.tsx
+++ app/milestones/page.tsx
@@ -3,29 +3,34 @@ import React, { FC } from 'react'
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import { redirect } from 'next/navigation'
 import type { Session } from 'next-auth'
-import type { Milestone } from '@prisma/client'
 import prisma from '@/prisma/db'
+import Helper from '@/_utils/helper'
 
 const Milestones: FC = async () => {
-  const data = await getMilestones()
+  const { success, data, error } = await getMilestones()
 
+  if (error) return <div>{String(error)}</div>
+  if (success && data!.length === 0) {
+    return (
+      <div>
+        There are no milestones associated to this account. Please add your
+        first milestone!
+      </div>
+    )
+  }
   return (
     <>
-      {data instanceof Error ? (
-        <div>{String(data)}</div>
-      ) : (
-        <div>
-          {data.map(({ id, title, content, type, document, date }) => (
-            <div key={id}>
-              <h2>{title}</h2>
-              <p>{content}</p>
-              <p>{type}</p>
-              <p>{date.toDateString()}</p>
-              <p>{document}</p>
-            </div>
-          ))}
-        </div>
-      )}
+      <div>
+        {data!.map(({ id, title, content, type, document, date }) => (
+          <div key={id}>
+            <h2>{title}</h2>
+            <p>{content}</p>
+            <p>{type}</p>
+            <p>{date.toDateString()}</p>
+            <p>{document}</p>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
@@ -34,15 +39,14 @@ export default Milestones
 
 const getSession = async (): Promise<Session> => {
   const session = await getServerSession(authOptions)
-  console.log('milestones page session: ', session)
-
   if (!session) {
     redirect('/signin?redirect=milestones')
   }
+
   return session
 }
 
-const getMilestones = async (): Promise<Milestone[] | Error> => {
+const getMilestones = async (): Promise<GetMilestoneApiResponse> => {
   const session = await getSession()
   try {
     const user = await prisma.user.findFirstOrThrow({
@@ -60,9 +64,17 @@ const getMilestones = async (): Promise<Milestone[] | Error> => {
       },
     })
 
-    return milestones
+    return {
+      success: true,
+      data: milestones,
+      error: null,
+    }
   } catch (err) {
     console.log('milestones page user:', err)
-    return err as Error
+    return {
+      success: false,
+      data: null,
+      error: Helper.sanitizeErrorMessage(String(err)),
+    }
   }
 }
