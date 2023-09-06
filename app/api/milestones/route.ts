c197fe37ce9e6724a3de7aab79294de2edfc8d70
Brian <brian.noh95@gmail.com>
2023-09-06 00:30:03 -0400

    specify document instanceof for if statement

--- app/api/milestones/route.ts
+++ app/api/milestones/route.ts
@@ -21,7 +21,7 @@ export async function POST(req: Request) {
 
     let documentPath: string = ''
 
-    if (milestoneData.document) {
+    if (milestoneData.document instanceof File) {
       documentPath = await uploadDocumentHandler(milestoneData.document)
     }
 
