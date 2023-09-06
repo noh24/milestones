2f7d5de823dd75d4b7031bdf07d82bc58ada7545
Brian <brian.noh95@gmail.com>
2023-09-05 21:38:43 -0400

    add ensureUploadsDirectoyExists

--- app/api/milestones/_utils.ts
+++ app/api/milestones/_utils.ts
@@ -1,34 +1,35 @@
 import path from 'path'
 import crypto from 'crypto'
 import { writeFile } from 'fs/promises'
+import fs from 'fs'
+
 
 export const parseFormData = (formData: FormData) => ({
   title: formData.get('title') as string,
   content: formData.get('content') as string,
   type: formData.get('type') as string,
   date: new Date(formData.get('date') as string).toISOString(),
-  document: formData.get('document') as Blob | null
+  document: formData.get('document') as File | null
 })
 
-export const uploadDocumentHandler = async (document: Blob): Promise<string> => {
+export const uploadDocumentHandler = async (document: File): Promise<string> => {
   if (!validateDocumentType(document.type)) {
     throw new Error('Document type is not acceptable MIME type.')
   }
 
   if (!validateDocumentSize(document.size)) {
-    throw new Error('File is too large. Maximum file size is 5MB.')
+    throw new Error(`File is too large: ${convertByteToMb(document.size)} MB. Maximum file size is 5 MB.`)
   }
 
   const arrayBuffer = await document.arrayBuffer()
   const buffer = Buffer.from(arrayBuffer)
 
-  const documentPath = generateUploadPath() + generateFileExtension(document.type)
+  const documentPath = generateDocumentPath(document)
   await writeFile(documentPath, buffer)
-
   return documentPath
 }
 
-export const validateDocumentType = (type: string): boolean => {
+const validateDocumentType = (type: string): boolean => {
   const lowerCaseType = type.toLowerCase()
   const mimeTypes = [
     'application/msword',
@@ -41,11 +42,14 @@ export const validateDocumentType = (type: string): boolean => {
   return mimeTypes.includes(lowerCaseType)
 }
 
-export const validateDocumentSize = (documentSize: number): boolean => {
+const validateDocumentSize = (documentSize: number): boolean => {
   return documentSize <= 5 * 1024 * 1024 //max 5mb * 1024 kilobyte in mb * 1024 byte in kilobyte
 }
+const convertByteToMb = (documentSize: number): string => {
+  return (documentSize / 1024 / 1024).toFixed(2)
+}
 
-export const generateFileExtension = (type: string): string => {
+const generateRandomFileName = (document: File): string => {
   const mimeTypes = {
     'application/msword': '.doc',
     'application/vnd.openxmlformats-officedocument.wordprocessingml.document': '.docx',
@@ -55,10 +59,21 @@ export const generateFileExtension = (type: string): string => {
     'image/jpg': '.jpg'
   }
 
-  return mimeTypes[type as keyof typeof mimeTypes]
+  return `${document.name}_${crypto.randomUUID()}.${mimeTypes[document.type as keyof typeof mimeTypes]}`
 }
 
-export const generateUploadPath = (): string => {
-  const randomId = crypto.randomUUID()
-  return path.join(__dirname, '..', '..', '..', '..', '..', 'uploads', randomId)
+const generateDocumentPath = (document: File): string => {
+  const uploadPath = path.join(__dirname, '..', '..', '..', '..', '..', 'uploads')
+  ensureUploadsDirectoryExists(uploadPath)
+  return path.join(uploadPath, generateRandomFileName(document))
 }
+
+// if /uploads directory doesn't exist or is not a directory, create it
+const ensureUploadsDirectoryExists = (uploadPath: string): void => {
+  if (!fs.existsSync(uploadPath) || !fs.statSync(uploadPath).isDirectory()) {
+    fs.mkdirSync(uploadPath)
+  }
+}
+
+
+
