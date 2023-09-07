166e2ac9c13a0035c8c0a894eec0f189d589f51c
Brian <brian.noh95@gmail.com>
2023-09-07 01:50:29 -0400

    wip: create deleteMilestoneAndDocument mutation fn

---
+++ _components/MilestoneDeleteButton.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { useMutation } from '@tanstack/react-query'
+import React, { FC } from 'react'
+
+const MilestoneDeleteButton: FC<MilestoneDeleteData> = ({
+  id,
+  documentPath,
+}) => {
+  const mutation = useMutation(deleteMilestoneAndDocument)
+
+  const deleteMilestone = () => {
+    mutation.mutate({ id, documentPath })
+  }
+
+  return <button onClick={deleteMilestone}>Delete</button>
+}
+
+export default MilestoneDeleteButton
+
+const deleteMilestoneAndDocument = async ({
+  id,
+  documentPath,
+}: MilestoneDeleteData) => {
+  const res = await fetch('/api/milestone', {
+    method: 'DELETE',
+    body: JSON.stringify({ id, documentPath }),
+  })
+
+  const { success, data, error }: DeleteMilestoneApiResponse = await res.json()
+
+  if (res.ok) {
+    return data
+  } else {
+    throw new Error(error!)
+  }
+}
