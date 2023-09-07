8753f42fc457db1f8a4ae5a30a9ff456ace406d7
Brian <brian.noh95@gmail.com>
2023-09-06 23:08:26 -0400

    add delete milestone button for all milestones

--- app/milestones/page.tsx
+++ app/milestones/page.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import Link from 'next/link'
 import { getMilestones } from './_utils'
+import DeleteMilestoneButton from '@/_components/DeleteMilestoneButton'
 
 const Milestones: FC = async () => {
   const { success, data, error } = await getMilestones()
@@ -20,13 +21,14 @@ const Milestones: FC = async () => {
   return (
     <>
       <div>
-        {data!.map(({ id, title, content, type, date }) => (
+        {data!.map(({ id, title, content, type, date, document }) => (
           <div key={id}>
             <Link href={`/milestones/${id}`}>
               <h2>{title}</h2>
               <p>{type}</p>
               <p>{content}</p>
               <p>{date.toDateString()}</p>
+              <DeleteMilestoneButton id={id} documentPath={document} />
             </Link>
           </div>
         ))}
