0954bc5491aa29cb733bfc049143d8a6e6698179
Brian <brian.noh95@gmail.com>
2023-09-10 00:43:41 -0400

    opt to reload page after delete

--- app/milestones/MilestoneDeleteButton.tsx
+++ app/milestones/MilestoneDeleteButton.tsx
@@ -1,13 +1,20 @@
 'use client'
 
 import { useMutation } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
 import React, { FC } from 'react'
 
 const MilestoneDeleteButton: FC<MilestoneDeleteData> = ({ id }) => {
+  const router = useRouter()
+
   const mutation = useMutation(deleteMilestoneAndDocument)
 
   const onClickDelete = () => {
     mutation.mutate({ id })
+
+    // can't call revalidation api
+    // nextjs revalidatePath() only revalidates on next visit
+    router.refresh()
   }
 
   return <button onClick={onClickDelete}>Delete</button>
@@ -18,7 +25,7 @@ export default MilestoneDeleteButton
 export const deleteMilestoneAndDocument = async ({
   id,
 }: MilestoneDeleteData) => {
-  const res = await fetch('/api/milestone', {
+  const res = await fetch('/api/milestones', {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
@@ -29,8 +36,6 @@ export const deleteMilestoneAndDocument = async ({
   const { success, data, error }: DeleteMilestoneApiResponse = await res.json()
 
   if (res.ok) {
-    // todo: call revalidate api - pass in path, secret validation token
-    // todo: redirect back to milestones page
     return data
   } else {
     throw new Error(error!)
