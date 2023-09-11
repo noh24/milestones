1a13dd79085ede9b868a040cbe45a111ddb584d3
Brian Noh <brian.noh95@gmail.com>
2023-09-11 01:49:23 -0400

    add link to milestones edit page & pass milestone id as params

--- app/milestones/page.tsx
+++ app/milestones/page.tsx
@@ -30,6 +30,14 @@ const Milestones = async () => {
               <p>{new Date(date).toDateString()}</p>
             </Link>
             <MilestoneDeleteButton id={id} />
+            <Link
+              href={{
+                pathname: '/milestones/edit',
+                query: { id },
+              }}
+            >
+              Update
+            </Link>
           </div>
         ))}
       </div>
