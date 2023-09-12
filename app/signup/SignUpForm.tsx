229e08c125a03bc185d3c986f35581a7d1680fe7
Brian <brian.noh95@gmail.com>
2023-09-11 21:51:24 -0400

    extract client code into signup form comp

---
+++ app/signup/SignUpForm.tsx
@@ -0,0 +1,79 @@
+'use client'
+
+import { useMutation } from '@tanstack/react-query'
+import { useState } from 'react'
+import { signUpUser } from './_utils'
+import Loading from '../loading'
+import { useRouter } from 'next/navigation'
+
+export default function SignUpForm() {
+  const router = useRouter()
+
+  const [userData, setUserData] = useState<UserSignUpData>({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  const updateUserData =
+    (type: keyof UserSignUpData) =>
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setUserData((prevState) => ({ ...prevState, [type]: e.target.value }))
+    }
+
+  const mutation = useMutation(signUpUser)
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    mutation.mutate({ userData, router })
+  }
+
+  if (mutation.isLoading) return <Loading />
+  if (mutation.isSuccess) return <div>{mutation.data}</div>
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          name='name'
+          type='text'
+          placeholder='Enter your name'
+          required
+          value={userData.name}
+          onChange={updateUserData('name')}
+        />
+        <input
+          name='email'
+          type='email'
+          placeholder='Enter your email'
+          required
+          value={userData.email}
+          onChange={updateUserData('email')}
+        />
+        <input
+          name='password'
+          type='password'
+          placeholder='Enter your password'
+          required
+          value={userData.password}
+          onChange={updateUserData('password')}
+        />
+        <input
+          name='confirmPassword'
+          type='password'
+          placeholder='Re-enter your password'
+          required
+          value={userData.confirmPassword}
+          onChange={updateUserData('confirmPassword')}
+        />
+        <button
+          type='submit'
+          disabled={mutation.isLoading || mutation.isSuccess}
+        >
+          Create Account
+        </button>
+      </form>
+      <p>{mutation.isError ? (mutation.error as Error).message : null}</p>
+    </>
+  )
+}
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,87 +1,22 @@
-'use client'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
+import SignUpForm from './SignUpForm'
+import { Metadata } from 'next'
 
-import React, { useEffect, useState } from 'react'
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
-import { useMutation } from '@tanstack/react-query'
-import Loading from '../loading'
-import { signUpUser } from './_utils'
-
-const SignUp = () => {
-  const { status } = useSession()
-  const router = useRouter()
-
-  useEffect(() => {
-    if (status === 'authenticated') router.push('/')
-  }, [status, router])
-
-  const [userData, setUserData] = useState<UserSignUpData>({
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-  })
-
-  const updateUserData =
-    (type: keyof UserSignUpData) =>
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      setUserData((prevState) => ({ ...prevState, [type]: e.target.value }))
-    }
-
-  const mutation = useMutation(signUpUser)
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    mutation.mutate({ userData, router })
-  }
-
-  if (mutation.isLoading) return <Loading />
-  if (mutation.isSuccess) return <div>{mutation.data}</div>
-  return (
-    <>
-      <form onSubmit={onSubmit}>
-        <input
-          name='name'
-          type='text'
-          placeholder='Enter your name'
-          required
-          value={userData.name}
-          onChange={updateUserData('name')}
-        />
-        <input
-          name='email'
-          type='email'
-          placeholder='Enter your email'
-          required
-          value={userData.email}
-          onChange={updateUserData('email')}
-        />
-        <input
-          name='password'
-          type='password'
-          placeholder='Enter your password'
-          required
-          value={userData.password}
-          onChange={updateUserData('password')}
-        />
-        <input
-          name='confirmPassword'
-          type='password'
-          placeholder='Re-enter your password'
-          required
-          value={userData.confirmPassword}
-          onChange={updateUserData('confirmPassword')}
-        />
-        <button
-          type='submit'
-          disabled={mutation.isLoading || mutation.isSuccess}
-        >
-          Create Account
-        </button>
-      </form>
-      <p>{mutation.isError ? (mutation.error as Error).message : null}</p>
-    </>
-  )
+export const metadata: Metadata = {
+  title: 'Sign Up - Milestones',
 }
 
-export default SignUp
+const getSessionOrRedirect = async () => {
+  const session = await getServerSession(authOptions)
+
+  if (session) {
+    redirect('/')
+  }
+}
+
+export default async function SignUp() {
+  await getSessionOrRedirect()
+  return <SignUpForm />
+}
