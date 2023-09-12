f2a09928ec3ee20942b6b5fbcf75f39354bab609
Brian <brian.noh95@gmail.com>
2023-09-11 21:56:34 -0400

    extract getSessionOrRedirect to CustomSession class

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,22 +1,12 @@
-import { getServerSession } from 'next-auth'
-import { authOptions } from '../api/auth/[...nextauth]/route'
-import { redirect } from 'next/navigation'
 import SignUpForm from './SignUpForm'
 import { Metadata } from 'next'
+import CustomSession from '../_server_utils/customSession'
 
 export const metadata: Metadata = {
   title: 'Sign Up - Milestones',
 }
 
-const getSessionOrRedirect = async () => {
-  const session = await getServerSession(authOptions)
-
-  if (session) {
-    redirect('/')
-  }
-}
-
 export default async function SignUp() {
-  await getSessionOrRedirect()
+  await CustomSession.getSessionOrRedirect()
   return <SignUpForm />
 }
