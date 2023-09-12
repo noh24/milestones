66573adafdc6a73cfe2a41b7095c7383daee93bb
Brian <brian.noh95@gmail.com>
2023-09-11 22:55:41 -0400

    add metaData title

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,5 +1,10 @@
 import SignInForm from './SignInForm'
 import CustomSession from '../_server_utils/customSession'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Sign In - Milestones',
+}
 
 export default async function SignIn() {
   await CustomSession.getSessionAndRedirectIfSession()
