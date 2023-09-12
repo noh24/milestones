377b0d65ad879bc0e705c7ef0b4e7594f923f560
Brian <brian.noh95@gmail.com>
2023-09-11 22:29:48 -0400

    rename getSessionAndRedirectIfSession

--- app/_server_utils/customSession.ts
+++ app/_server_utils/customSession.ts
@@ -3,7 +3,7 @@ import { authOptions } from "../api/auth/[...nextauth]/route"
 import { redirect } from "next/navigation"
 
 export default class CustomSession {
-  static async getSessionOrRedirect() {
+  static async getSessionAndRedirectIfSession() {
     const session = await getServerSession(authOptions)
 
     if (session) {
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -7,6 +7,6 @@ export const metadata: Metadata = {
 }
 
 export default async function SignUp() {
-  await CustomSession.getSessionOrRedirect()
+  await CustomSession.getSessionAndRedirectIfSession()
   return <SignUpForm />
 }
