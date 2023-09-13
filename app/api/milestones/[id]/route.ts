10a04e3f7cf6014eeb5c157912c9301bad50022d
Brian <brian.noh95@gmail.com>
2023-09-13 02:19:28 -0400

    rm milestones [id] api directory

--- app/api/milestones/[id]/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server"
-
-export async function GET(req: Request, { params }: { params: { id: string | null } }) {
-  const id = params.id
-  console.log(id)
-
-  return NextResponse.json({ success: true, data: '', error: null })
-}
