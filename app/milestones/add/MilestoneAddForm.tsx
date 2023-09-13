e04e3b42a34504d30bcb40099973218764b91ebf
Brian <brian.noh95@gmail.com>
2023-09-13 01:58:24 -0400

    extract milestone add form to separate client comp

---
+++ app/milestones/add/MilestoneAddForm.tsx
@@ -0,0 +1,125 @@
+'use client'
+
+import { useMutation } from '@tanstack/react-query'
+import { useState } from 'react'
+import { createMilestoneAndRevalidate } from './_utils'
+import { useRouter } from 'next/navigation'
+import Loading from '@/app/loading'
+
+type TProps = {
+  userEmail: string
+}
+
+export default function MilestoneAddForm({ userEmail }: TProps) {
+  const router = useRouter()
+
+  const [milestoneData, setMilestoneData] = useState<MilestoneFormData>({
+    title: '',
+    content: '',
+    type: '',
+    date: '',
+    document: '',
+    userEmail: userEmail,
+  })
+
+  const onUpdateMilestoneData =
+    (type: keyof MilestoneFormData) =>
+    (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
+      setMilestoneData((prevState) => ({
+        ...prevState,
+        [type]: event.target.value,
+      }))
+
+  const onUpdateMilestoneDocument = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) =>
+    setMilestoneData((prevState) => ({
+      ...prevState,
+      document: event.target.files![0],
+    }))
+
+  const mutation = useMutation(createMilestoneAndRevalidate)
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    mutation.mutate({ milestoneData, router })
+  }
+
+  if (mutation.isLoading) {
+    return <Loading />
+  }
+
+  if (mutation.isSuccess) {
+    return <div>{mutation.data}</div>
+  }
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          name='title'
+          type='text'
+          placeholder='Title'
+          required
+          value={milestoneData.title}
+          onChange={onUpdateMilestoneData('title')}
+        />
+        <label>
+          Select Date:
+          <input
+            name='date'
+            type='date'
+            required
+            value={milestoneData.date}
+            onChange={onUpdateMilestoneData('date')}
+          />
+        </label>
+        <textarea
+          name='content'
+          placeholder='Enter details'
+          required
+          value={milestoneData.content}
+          onChange={onUpdateMilestoneData('content')}
+        />
+        <label>
+          <input
+            name='type'
+            type='radio'
+            required
+            value='professional'
+            onChange={onUpdateMilestoneData('type')}
+          />
+          Professional
+        </label>
+        <label>
+          <input
+            name='type'
+            type='radio'
+            value='personal'
+            onChange={onUpdateMilestoneData('type')}
+          />
+          Personal
+        </label>
+        <label>
+          Upload a document (Maximum file size: 5MB):
+          <span>.pdf</span>
+          <span>.doc / .docx</span>
+          <span>.jpeg / .png </span>
+          <input
+            name='document'
+            type='file'
+            accept='.doc,.docx,.pdf,.jpeg,.png,.jpg'
+            onChange={onUpdateMilestoneDocument}
+          />
+        </label>
+        <button
+          type='submit'
+          disabled={mutation.isLoading || mutation.isSuccess}
+        >
+          Add Milestone
+        </button>
+      </form>
+      <p>{mutation.isError ? (mutation.error as Error).message : null}</p>
+    </>
+  )
+}
--- app/milestones/add/page.tsx
+++ app/milestones/add/page.tsx
@@ -1,128 +1,18 @@
-'use client'
-
-import React, { useEffect } from 'react'
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import { createMilestoneAndRevalidate } from './_utils'
-import { useMutation } from '@tanstack/react-query'
-import Loading from '@/app/loading'
+import { redirect } from 'next/navigation'
 import { Metadata } from 'next'
+import CustomSession from '@/app/_server_utils/customSession'
+import MilestoneAddForm from './MilestoneAddForm'
 
 export const metadata: Metadata = {
   title: 'Add Milestones - Milestones',
 }
 
-export default function Page() {
-  const { data: session, status } = useSession()
-  const router = useRouter()
+export default async function Page() {
+  const session = await CustomSession.getServerSession()
 
-  useEffect(() => {
-    if (status !== 'authenticated')
-      router.push('/signin?redirect=milestones/add')
-  }, [status, router])
-
-  const [milestoneData, setMilestoneData] = useState<MilestoneFormData>({
-    title: '',
-    content: '',
-    type: '',
-    date: '',
-    document: '',
-    userEmail: session?.user?.email!,
-  })
-
-  const onUpdateMilestoneData =
-    (type: keyof MilestoneFormData) =>
-    (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
-      setMilestoneData((prevState) => ({
-        ...prevState,
-        [type]: event.target.value,
-      }))
-
-  const onUpdateMilestoneDocument = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) =>
-    setMilestoneData((prevState) => ({
-      ...prevState,
-      document: event.target.files![0],
-    }))
-
-  const mutation = useMutation(createMilestoneAndRevalidate)
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    mutation.mutate({ milestoneData, router })
+  if (!session) {
+    redirect('/signin?redirect=milestones/add')
   }
 
-  if (mutation.isLoading) return <Loading />
-  if (mutation.isSuccess) return <div>{mutation.data}</div>
-  return (
-    <>
-      <form onSubmit={onSubmit}>
-        <input
-          name='title'
-          type='text'
-          placeholder='Title'
-          required
-          value={milestoneData.title}
-          onChange={onUpdateMilestoneData('title')}
-        />
-        <label>
-          Select Date:
-          <input
-            name='date'
-            type='date'
-            required
-            value={milestoneData.date}
-            onChange={onUpdateMilestoneData('date')}
-          />
-        </label>
-        <textarea
-          name='content'
-          placeholder='Enter details'
-          required
-          value={milestoneData.content}
-          onChange={onUpdateMilestoneData('content')}
-        />
-        <label>
-          <input
-            name='type'
-            type='radio'
-            required
-            value='professional'
-            onChange={onUpdateMilestoneData('type')}
-          />
-          Professional
-        </label>
-        <label>
-          <input
-            name='type'
-            type='radio'
-            value='personal'
-            onChange={onUpdateMilestoneData('type')}
-          />
-          Personal
-        </label>
-        <label>
-          Upload a document (Maximum file size: 5MB):
-          <span>.pdf</span>
-          <span>.doc / .docx</span>
-          <span>.jpeg / .png </span>
-          <input
-            name='document'
-            type='file'
-            accept='.doc,.docx,.pdf,.jpeg,.png,.jpg'
-            onChange={onUpdateMilestoneDocument}
-          />
-        </label>
-        <button
-          type='submit'
-          disabled={mutation.isLoading || mutation.isSuccess}
-        >
-          Add Milestone
-        </button>
-      </form>
-      <p>{mutation.isError ? (mutation.error as Error).message : null}</p>
-    </>
-  )
+  return <MilestoneAddForm userEmail={session?.user?.email!} />
 }
