98374a274ff4b373a23e9d06a902f394d674317e
Brian <brian.noh95@gmail.com>
2023-09-13 02:42:29 -0400

    wip: create MilestoneEditForm

---
+++ app/milestones/[id]/edit/MilestoneEditForm.tsx
@@ -0,0 +1,122 @@
+'use client'
+
+import Loading from '@/app/loading'
+import { useMutation } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import { createMilestoneAndRevalidate } from '../../add/_utils'
+import { Milestone } from '@prisma/client'
+import { MilestoneFormData } from '@/types/types'
+
+type TProps = {
+  userEmail: string
+  milestone: Milestone
+}
+
+export default function MilestoneEditForm({ userEmail, milestone }: TProps) {
+  const router = useRouter()
+
+  const [milestoneData, setMilestoneData] = useState<MilestoneFormData>({
+    title: milestone.title,
+    content: milestone.content,
+    type: milestone.type,
+    date: String(milestone.date),
+    document: milestone.document ? milestone.document : '',
+    userEmail,
+  })
+
+  const onUpdateMilestoneData =
+    (type: keyof MilestoneFormData) =>
+    (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
+      setMilestoneData((prevState) => ({
+        ...prevState,
+        [type]: event.target.value,
+      }))
+
+  const onUpdateMilestoneDocument = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) =>
+    setMilestoneData((prevState) => ({
+      ...prevState,
+      document: event.target.files![0],
+    }))
+
+  const mutation = useMutation(createMilestoneAndRevalidate)
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    mutation.mutate({ milestoneData, router })
+  }
+
+  if (mutation.isLoading) return <Loading />
+  if (mutation.isSuccess) return <div>{mutation.data}</div>
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          name='title'
+          type='text'
+          placeholder='Title'
+          required
+          value={milestoneData.title}
+          onChange={onUpdateMilestoneData('title')}
+        />
+        <label>
+          Select Date:
+          <input
+            name='date'
+            type='date'
+            required
+            value={milestoneData.date}
+            onChange={onUpdateMilestoneData('date')}
+          />
+        </label>
+        <textarea
+          name='content'
+          placeholder='Enter details'
+          required
+          value={milestoneData.content}
+          onChange={onUpdateMilestoneData('content')}
+        />
+        <label>
+          <input
+            name='type'
+            type='radio'
+            required
+            value='professional'
+            onChange={onUpdateMilestoneData('type')}
+          />
+          Professional
+        </label>
+        <label>
+          <input
+            name='type'
+            type='radio'
+            value='personal'
+            onChange={onUpdateMilestoneData('type')}
+          />
+          Personal
+        </label>
+        <label>
+          Upload a document (Maximum file size: 5MB):
+          <span>.pdf</span>
+          <span>.doc / .docx</span>
+          <span>.jpeg / .png </span>
+          <input
+            name='document'
+            type='file'
+            accept='.doc,.docx,.pdf,.jpeg,.png,.jpg'
+            onChange={onUpdateMilestoneDocument}
+          />
+        </label>
+        <button
+          type='submit'
+          disabled={mutation.isLoading || mutation.isSuccess}
+        >
+          Add Milestone
+        </button>
+      </form>
+      <p>{mutation.isError ? (mutation.error as Error).message : null}</p>
+    </>
+  )
+}
