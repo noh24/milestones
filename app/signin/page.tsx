fe6041ca5c5a6554a8a21b1a707a67cdb86da402
Brian <brian.noh95@gmail.com>
2023-09-13 00:55:14 -0400

    redirect if session exist

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,13 +2,20 @@ import SignInForm from './SignInForm'
 import CustomSession from '../_server_utils/customSession'
 import { Metadata } from 'next'
 import { getAllProviders } from './_utils'
+import { redirect } from 'next/navigation'
 
 export const metadata: Metadata = {
   title: 'Sign In - Milestones',
 }
 
 export default async function Page() {
-  await CustomSession.getSessionAndRedirectIfSession()
+  const session = await CustomSession.getServerSession()
+
+  if (session) {
+    redirect('/')
+  }
+
   const providers = await getAllProviders()
+
   return <SignInForm providers={providers} />
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,12 +1,18 @@
 import SignUpForm from './SignUpForm'
 import { Metadata } from 'next'
 import CustomSession from '../_server_utils/customSession'
+import { redirect } from 'next/navigation'
 
 export const metadata: Metadata = {
   title: 'Sign Up - Milestones',
 }
 
 export default async function Page() {
-  await CustomSession.getSessionAndRedirectIfSession()
+  const session = await CustomSession.getServerSession()
+
+  if (session) {
+    redirect('/')
+  }
+
   return <SignUpForm />
 }
