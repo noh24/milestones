9c90196415c8cef5fc53119232d4e3c956a09d26
Brian <brian.noh95@gmail.com>
2023-09-17 21:33:08 -0400

    refactor sanitizeDocumentName

--- app/_utils/helper.ts
+++ app/_utils/helper.ts
@@ -5,12 +5,7 @@ export default class Helper {
   }
 
   static sanitizeDocumentName(documentName: string): string {
-    let splitDocumentName = documentName.split('/')
-    const indexOfUploads = splitDocumentName.indexOf('uploads')
-    splitDocumentName.splice(0, indexOfUploads + 1)
-    splitDocumentName = splitDocumentName.join('').split('.')
-    splitDocumentName = splitDocumentName.join('').split('_')
-    return splitDocumentName.slice(0, -1).join('_')
+    return 'C://Fake//Path//' + documentName.slice(49).split('_').slice(0, -1).join('_')
   }
 
   static async revalidatePath({ path }: { path: string }) {
