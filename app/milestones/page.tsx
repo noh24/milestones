4f33981be28e7db8f273ab4a29af6097517b0389
Brian <brian.noh95@gmail.com>
2023-09-18 01:20:42 -0400

    opt out of caching

--- app/milestones/page.tsx
+++ app/milestones/page.tsx
@@ -12,6 +12,9 @@ export const metadata: Metadata = {
   title: 'All Milestones - Milestones',
 }
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export default async function Page() {
   const session = await CustomSession.getServerSession()
 
